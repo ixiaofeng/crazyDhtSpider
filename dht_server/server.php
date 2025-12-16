59856e576bb0c7141d41aff0cf441fd5e684b012
ixiaofeng <ixiaofeng.com@gmail.com>
2025-12-16 13:30:52 +0800

    Add master process name for dht_server and update README

--- dht_server/server.php
+++ dht_server/server.php
@@ -20,6 +20,13 @@ Func::Logs(date('Y-m-d H:i:s', time()) . " - 服务启动..." . PHP_EOL, 1);
 $serv = new Swoole\Server('0.0.0.0', 2345, SWOOLE_PROCESS, SWOOLE_SOCK_UDP);
 $serv->set($config);
 Swoole\Coroutine::set(['hook_flags' => SWOOLE_HOOK_ALL]);
+
+// 注册启动事件，设置主进程名称
+$serv->on('start', function ($serv) {
+    // 设置主进程名称，与客户端区分开
+    swoole_set_process_name("php_dht_server_master");
+});
+
 $serv->on('WorkerStart', 'MySwoole::workStart');
 $serv->on('Packet', 'MySwoole::packet');
 $serv->on('WorkerExit', 'MySwoole::workerExit');
